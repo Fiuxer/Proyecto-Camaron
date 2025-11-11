160b876d0a396877522427fb06fc4b3857c15903
Fiuxer <leonardinoeb@gmail.com>
2025-11-11 10:00:36 -0600

    Gang I did like main page and also fixed login and changed everything to gridboxes and also fixed the css and also made the functionality of the perks and membresias and stuff

---
+++ project/frontend/main/main.js
@@ -0,0 +1,11 @@
+
+
+function returnToLogin() {
+  window.location.href = "/Proyecto-Camaron/";
+}
+
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector(".ad-div").style.display = "grid";
+  }, 2000);
+};
