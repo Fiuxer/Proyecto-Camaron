f74c8e8e47a9f1db4a03381dccff18365ac9d085
Fiuxer <leonardinoeb@gmail.com>
2025-11-14 09:00:55 -0600

    Animations for the homepage

--- project/frontend/homepage/home.js
+++ project/frontend/homepage/home.js
@@ -90,3 +90,25 @@ function SplitText(selector) {
 
   return chars;
 }
+
+const faders = document.querySelectorAll('.fade');
+
+const appearOptions = {
+  threshold: 0.1,
+  rootMargin: "0px 0px -100px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('show');
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
