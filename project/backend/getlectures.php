1e5f7db2874b4ac1dce305d0696036ce4f3dfd95
Fiuxer <leonardinoeb@gmail.com>
2025-11-20 15:16:02 -0600

    Fuzzy search kinda like just stripos but same shit

--- project/backend/getlectures.php
+++ project/backend/getlectures.php
@@ -4,14 +4,25 @@ include "db.php";
 
 $query = "SELECT * FROM lecciones";
 $result = $conn->query($query);
+$search = file_get_contents("php://input");
+$search == "" ? $doSearch = false : $doSearch = true;
 
 $lectures = [];
 
 while ($row = $result->fetch_assoc()) {
-  $lectures [] = [
-    "id"=> $row["id"],
-    "name"=> $row["nombre"]
-  ];
+  if ($doSearch) {
+    if (stripos($row["nombre"],$search) !== false) {
+      $lectures [] = [
+        "id"=> $row["id"],
+        "name"=> $row["nombre"]
+      ];
+    }
+  } else {
+    $lectures [] = [
+      "id"=> $row["id"],
+      "name"=> $row["nombre"]
+    ];
+  }
 }
 
 header("Content-Type: application/json");
